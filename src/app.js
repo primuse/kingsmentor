7af9e8ce68adbcd0a639289d7742f66427018e52
Tikuochukwu Okoye <okoyetiku@gmail.com>
2019-06-10 10:41:40 +0100

    task: setup Project

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react';
+import {
+  BrowserRouter, Route, Switch
+} from 'react-router-dom';
+import Home from './components/home';
+
+const App = () => (
+  <BrowserRouter>
+    <Fragment>
+      <Switch>
+        <Route path="/" component={Home} />
+      </Switch>
+    </Fragment>
+  </BrowserRouter>
+);
+
+export default App;
---
+++ src/components/home.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Home = () => {
+  const name = 'Okoye';
+
+  return <h1>Tiku {name}</h1>;
+};
+export default Home;
